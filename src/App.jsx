33ebfa76463a1d65ce92c09a4deca476570df4b9
imon018 <imon5439@gmail.com>
2026-07-06 00:21:13 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import { Routes, Route } from "react-router-dom";
 
-function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-primary">
-        Welcome to Dream Mode
-      </h1>
-    </div>
-  );
-}
+import MainLayout from "./layouts/MainLayout";
+
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+
+      <Route element={<MainLayout />}>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/shop"
+          element={<Shop />}
+        />
+
+        <Route
+          path="/cart"
+          element={<Cart />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+      </Route>
+
     </Routes>
   );
 }
