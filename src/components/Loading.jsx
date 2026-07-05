dac29894587c8add7428cfffe1f6a7bc151f7075
imon018 <imon5439@gmail.com>
2026-07-06 00:29:27 +0600

    Loading.jsx

---
+++ src/components/Loading.jsx
@@ -0,0 +1,17 @@
+export default function Loading() {
+
+  return (
+
+    <div className="flex justify-center items-center min-h-screen">
+
+      <h2 className="text-xl font-semibold">
+
+        Loading...
+
+      </h2>
+
+    </div>
+
+  );
+
+}
