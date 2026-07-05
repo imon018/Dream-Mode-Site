04306b6d91ee661169e9b5a88fad7bb503aeb1df
imon018 <imon5439@gmail.com>
2026-07-06 00:32:39 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-
-import MainLayout from "./layouts/MainLayout";
-
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
+import AppRoutes from "./routes/AppRoutes";
+import AuthProvider from "./context/AuthContext";
 
 export default function App() {
   return (
-    <Routes>
-
-      <Route element={<MainLayout />}>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/shop"
-          element={<Shop />}
-        />
-
-        <Route
-          path="/cart"
-          element={<Cart />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-      </Route>
-
-    </Routes>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   );
 }
