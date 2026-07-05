8e0de96b46e717d2695a00b385e98c52b6d4364c
Imon Ahmed <imon5439@gmail.com>
2026-07-06 01:10:02 +0600

    WishlistContext.jsx

---
+++ src/context/WishlistContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const WishlistContext = createContext();
+
+export default function WishlistProvider({ children }) {
+  const [wishlist, setWishlist] = useState([]);
+
+  const addToWishlist = (product) => {
+    setWishlist((prev) => [...prev, product]);
+  };
+
+  const removeFromWishlist = (id) => {
+    setWishlist((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  return (
+    <WishlistContext.Provider
+      value={{ wishlist, addToWishlist, removeFromWishlist }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  );
+}
