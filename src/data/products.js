cc4e6c6e7fc8f21d19aa2c0184cf8a0aaf3e70e4
imon018 <imon5439@gmail.com>
2026-07-06 00:55:04 +0600

    products.js

---
+++ src/data/products.js
@@ -0,0 +1,28 @@
+const products = [
+
+{
+id:1,
+
+name:"Premium Watch",
+
+price:2500,
+
+image:"https://picsum.photos/500?1"
+
+},
+
+{
+
+id:2,
+
+name:"Wireless Headphone",
+
+price:3200,
+
+image:"https://picsum.photos/500?2"
+
+}
+
+];
+
+export default products;
