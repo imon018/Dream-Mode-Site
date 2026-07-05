01b61042ad4b8467e5a6b158985b7062f38f8bfd
Imon Ahmed <imon5439@gmail.com>
2026-07-06 05:20:30 +0600

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-export default function Footer() {
+import { siteConfig } from "../config/siteConfig";
+default function Footer() {
   return (
     <footer className="bg-slate-900 text-white mt-16">
 
@@ -9,9 +10,24 @@ export default function Footer() {
         </h2>
 
         <p className="mt-3 text-gray-300">
-          AI Powered E-Commerce Platform
+          Powered By 'Dream Mode Fashion'
         </p>
+        <p className="mt-4 text-gray-300">
+      📞 {siteConfig.phone}
+</p>
 
+<p className="text-gray-300">
+  ✉️ {siteConfig.email}
+</p>
+
+<a
+  href={siteConfig.facebook}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="block mt-2 text-blue-400 hover:text-blue-300"
+>
+  Facebook Page
+</a>
         <p className="mt-6 text-sm text-gray-400">
           © 2026 Dream Mode. All Rights Reserved.
         </p>
