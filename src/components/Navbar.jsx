09bb08d37350208e64a139b0b7970e77bea91775
Imon Ahmed <imon5439@gmail.com>
2026-07-06 01:54:19 +0600

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAuth from "../hooks/useAuth";
 
 export default function Navbar() {
   const { cart } = useCart();
+  const { user } = useAuth();
 
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
@@ -11,15 +13,20 @@ export default function Navbar() {
         Dream Mode
       </Link>
 
-      <div className="flex gap-6 items-center">
+      <div className="flex gap-5 items-center">
 
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
         <Link to="/wishlist">Wishlist</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/cart">
-          Cart ({cart.length})
-        </Link>
+        <Link to="/cart">Cart ({cart.length})</Link>
+
+        {user ? (
+          <Link to="/profile" className="text-green-600">
+            {user.email.split("@")[0]}
+          </Link>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
 
       </div>
 
