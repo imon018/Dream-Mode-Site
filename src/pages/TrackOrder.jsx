74937cb36680ed23a0f8250296e1fb0eecb002b9
imon018 <imon5439@gmail.com>
2026-07-06 00:39:05 +0600

    TrackOrder.jsx

---
+++ src/pages/TrackOrder.jsx
@@ -0,0 +1,13 @@
+export default function TrackOrder() {
+  return (
+    <div className="max-w-5xl mx-auto px-6 py-12">
+      <h1 className="text-4xl font-bold">
+        Track Order
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+        Track your order status here.
+      </p>
+    </div>
+  );
+}
