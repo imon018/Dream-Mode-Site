200d5be4d1137550c9658b73bc5068a28f6fb9e3
Imon Ahmed <imon5439@gmail.com>
2026-07-06 05:05:01 +0600

    siteConfig.js

---
+++ src/config/siteConfig.js
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  appName: import.meta.env.VITE_APP_NAME,
+
+  email: import.meta.env.VITE_BUSINESS_EMAIL,
+
+  phone: import.meta.env.VITE_BUSINESS_PHONE,
+
+  whatsapp: `https://wa.me/${import.meta.env.VITE_WHATSAPP_NUMBER}?text=Hello%20Dream%20Mode,%20I'm%20interested%20in%20your%20products.`,
+
+  facebook: import.meta.env.VITE_FACEBOOK_URL,
+};
