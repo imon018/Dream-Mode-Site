2e9339fdc0f833fc8f7174bc1ce52794959fbe03
Imon Ahmed <imon5439@gmail.com>
2026-07-06 01:53:25 +0600

    Wishlist.jsx

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,13 +1,5 @@
-export default function Wishlist() {
-  return (
-    <div className="max-w-7xl mx-auto px-6 py-12">
-      <h1 className="text-4xl font-bold">
-        Wishlist
-      </h1>
+import WishlistPage from "../components/WishlistPage";
 
-      <p className="mt-4 text-gray-600">
-        Saved products will appear here.
-      </p>
-    </div>
-  );
+export default function Wishlist() {
+  return <WishlistPage />;
 }
