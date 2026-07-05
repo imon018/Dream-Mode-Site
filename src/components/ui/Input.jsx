79931d638e2f5423804ff5ad12562991cba9ebd8
imon018 <imon5439@gmail.com>
2026-07-06 00:53:27 +0600

    Input.jsx

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,7 @@
+export default function Input(props) {
+  return (
+    <input
+      {...props}
+      className="w-full border rounded-xl px-4 py-3 outline-none focus:ring-2 focus:ring-primary"
+    />
+}
