24574265d3db942c542b1e1b5ccda6609b19e840
Imon Ahmed <imon5439@gmail.com>
2026-07-06 01:51:34 +0600

    Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,5 @@
+import CartPage from "../components/CartPage";
+
 export default function Cart() {
-  return (
-    <div className="max-w-7xl mx-auto px-6 py-12">
-
-      <h1 className="text-4xl font-bold">
-        Shopping Cart
-      </h1>
-
-      <p className="mt-3 text-gray-600">
-        Your selected products will appear here.
-      </p>
-
-    </div>
-  );
+  return <CartPage />;
 }
