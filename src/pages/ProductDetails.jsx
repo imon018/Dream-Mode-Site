3956909d097c75f6d2371acc5d53f12f1efe9ad1
Imon Ahmed <imon5439@gmail.com>
2026-07-06 01:13:08 +0600

    ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,15 +1,5 @@
+import ProductDetailsView from "../components/ProductDetailsView";
+
 export default function ProductDetails() {
-  return (
-    <div className="max-w-7xl mx-auto px-6 py-12">
-
-      <h1 className="text-4xl font-bold">
-        Product Details
-      </h1>
-
-      <p className="mt-4 text-gray-600">
-        Product information will appear here.
-      </p>
-
-    </div>
-  );
+  return <ProductDetailsView />;
 }
