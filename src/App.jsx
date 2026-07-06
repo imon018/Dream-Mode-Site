e373f2acc33bbf3d8e259d9c0bdb63279ef65dd5
Imon Ahmed <imon5439@gmail.com>
2026-07-07 01:47:41 +0600

    Wrap app with WishlistProvider and Toaster for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import FloatingWhatsApp from "./components/FloatingWhatsApp";
 import AppRoutes from "./routes/AppRoutes";
 import AuthProvider from "./context/AuthContext";
 import CartProvider from "./context/CartContext";
+import WishlistProvider from "./context/WishlistContext";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <AppRoutes />
-        <FloatingWhatsApp />
+        <WishlistProvider>
+          <AppRoutes />
+          <FloatingWhatsApp />
+          <Toaster position="top-right" />
+        </WishlistProvider>
       </CartProvider>
     </AuthProvider>
   );
