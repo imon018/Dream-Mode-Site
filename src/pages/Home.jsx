69465b87afa16d7cde59e3487e4b311cb764ac97
Imon Ahmed <imon5439@gmail.com>
2026-07-06 17:11:22 +0600

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import Hero from "../components/Hero";
 import FeaturedProducts from "../components/FeaturedProducts";
+import CategoryGrid from "../components/CategoryGrid";
+import OfferBanner from "../components/OfferBanner";
+import AnimatedWrapper from "../components/ui/AnimatedWrapper";
 
 export default function Home() {
   return (
-    <>
+    <AnimatedWrapper>
       <Hero />
       <FeaturedProducts />
-    </>
+      <CategoryGrid />
+      <OfferBanner />
+    </AnimatedWrapper>
   );
 }
