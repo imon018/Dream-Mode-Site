dc69ae87f54e5a15d676b2548086bb383d20a44f
Imon Ahmed <imon5439@gmail.com>
2026-07-06 17:18:58 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import FloatingWhatsApp from "./components/FloatingWhatsApp";
 import AppRoutes from "./routes/AppRoutes";
 import AuthProvider from "./context/AuthContext";
+import CartProvider from "./context/CartContext";
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppRoutes />
-      <FloatingWhatsApp />
+      <CartProvider>
+        <AppRoutes />
+        <FloatingWhatsApp />
+      </CartProvider>
     </AuthProvider>
   );
 }
