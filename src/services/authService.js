65c8d77c6b640cc59dd582d612e18a3a917ddab4
Imon Ahmed <imon5439@gmail.com>
2026-07-07 19:28:18 +0600

    Refactor register function in authService
    Refactor register function to simplify error handling and streamline code.

--- src/services/authService.js
+++ src/services/authService.js
@@ -13,28 +13,15 @@ import {
 import { auth } from "../firebase/auth";
 import { db } from "../firebase/firestore";
 
-export const login = (email, password) =>
-  signInWithEmailAndPassword(auth, email, password);
-
 export const register = async (email, password) => {
-  try {
-    const result = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
 
-    await setDoc(doc(db, "users", result.user.uid), {
-      email: result.user.email,
-      role: "user",
-      createdAt: serverTimestamp(),
-    });
+  const result = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
 
-    return result;
-  } catch (err) {
-    console.error("Register Error:", err);
-    throw err;
-  }
+  return result;
 };
 
 export const logout = () => signOut(auth);
