f0357ec699a52e1f2c9e010dbf6e65fca33227f7
Imon Ahmed <imon5439@gmail.com>
2026-07-07 18:23:05 +0600

    Update register function to save user in Firestore
    Refactor register function to use async/await and store user data in Firestore.

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,12 +4,33 @@ import {
   signOut
 } from "firebase/auth";
 
+import {
+  doc,
+  setDoc,
+  serverTimestamp
+} from "firebase/firestore";
+
 import { auth } from "../firebase/auth";
+import { db } from "../firebase/firestore";
 
 export const login = (email, password) =>
   signInWithEmailAndPassword(auth, email, password);
 
-export const register = (email, password) =>
-  createUserWithEmailAndPassword(auth, email, password);
+export const register = async (email, password) => {
+
+  const result = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  await setDoc(doc(db, "users", result.user.uid), {
+    email: result.user.email,
+    role: "user",
+    createdAt: serverTimestamp()
+  });
+
+  return result;
+};
 
 export const logout = () => signOut(auth);
