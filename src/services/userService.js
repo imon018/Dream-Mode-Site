a15764abc97546ad394390d43204f5172f7ed14e
Imon Ahmed <imon5439@gmail.com>
2026-07-09 07:23:38 +0600

    Refactor user profile functions with timestamps
    Refactor user profile functions to improve readability and include server timestamps for created and updated profiles.

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,41 +3,132 @@ import {
   getDoc,
   setDoc,
   updateDoc,
+  serverTimestamp,
 } from "firebase/firestore";
 
 import { db } from "../firebase/firestore";
 
-export const getUserProfile = async (uid) => {
-  const userRef = doc(db, "users", uid);
 
-  const snapshot = await getDoc(userRef);
+
+export const getUserProfile = async (uid) => {
+
+
+  const userRef =
+    doc(
+      db,
+      "users",
+      uid
+    );
+
+
+  const snapshot =
+    await getDoc(userRef);
+
+
 
   if (!snapshot.exists()) {
+
     return null;
+
   }
 
+
+
   return {
+
     id: snapshot.id,
+
     ...snapshot.data(),
+
   };
+
+
 };
+
+
+
+
+
 
 export const createUserProfile = async (
   uid,
   data
 ) => {
-  const userRef = doc(db, "users", uid);
 
-  await setDoc(userRef, data, {
-    merge: true,
-  });
+
+  const userRef =
+    doc(
+      db,
+      "users",
+      uid
+    );
+
+
+
+  await setDoc(
+
+    userRef,
+
+    {
+
+      ...data,
+
+      createdAt:
+        serverTimestamp(),
+
+    },
+
+    {
+
+      merge: true,
+
+    }
+
+  );
+
+
 };
+
+
+
+
+
 
 export const updateUserProfile = async (
   uid,
   data
 ) => {
-  const userRef = doc(db, "users", uid);
 
-  await updateDoc(userRef, data);
+
+  const userRef =
+    doc(
+      db,
+      "users",
+      uid
+    );
+
+
+
+  await setDoc(
+
+    userRef,
+
+    {
+
+      ...data,
+
+      updatedAt:
+        serverTimestamp(),
+
+    },
+
+    {
+
+      merge: true,
+
+    }
+
+  );
+
+
 };
