4d2032de90c9dff8f663ef36910bb11f9935954f
Imon Ahmed <imon5439@gmail.com>
2026-07-09 19:46:27 +0600

    Implement Firestore banner service functions

---
+++ src/services/firestoreBannerService.js
@@ -0,0 +1,44 @@
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
+import { db } from "../firebase/firestore";
+
+const bannerRef =
+  collection(db, "heroBanners");
+
+export const addBanner = async (
+  banner
+) => {
+  await addDoc(
+    bannerRef,
+    banner
+  );
+};
+
+export const getBanners = async () => {
+  const snapshot =
+    await getDocs(bannerRef);
+
+  return snapshot.docs.map(
+    (doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })
+  );
+};
+
+export const deleteBanner =
+  async (id) => {
+    await deleteDoc(
+      doc(
+        db,
+        "heroBanners",
+        id
+      )
+    );
+  };
