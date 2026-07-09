c6ead9b118a6d8bd4085f98345b163c68cce64f5
Imon Ahmed <imon5439@gmail.com>
2026-07-09 20:14:16 +0600

    Refactor Home component to update sections
    Removed Testimonials, DeliveryInfo, and OfferBanner components. Added StatsSection and TrustBanner components.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,24 @@
 import Hero from "../components/Hero";
 import FeaturedProducts from "../components/FeaturedProducts";
 import WhyChooseUs from "../components/WhyChooseUs";
-import Testimonials from "../components/Testimonials";
-import DeliveryInfo from "../components/DeliveryInfo";
-import OfferBanner from "../components/OfferBanner";
+import StatsSection from "../components/StatsSection";
+import TrustBanner from "../components/TrustBanner";
 import AnimatedWrapper from "../components/ui/AnimatedWrapper";
 
 export default function Home() {
   return (
     <AnimatedWrapper>
+
       <Hero />
+
       <FeaturedProducts />
+
       <WhyChooseUs />
-      <Testimonials />
-      <DeliveryInfo />
-      <OfferBanner />
+
+      <StatsSection />
+
+      <TrustBanner />
+
     </AnimatedWrapper>
   );
 }
