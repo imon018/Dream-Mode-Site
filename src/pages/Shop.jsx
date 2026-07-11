9123f0fd6a909c7f32ebce3927a7886e819da6a1
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-12 05:23:09 +0600

    Update className for grid layout in Shop component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -371,14 +371,14 @@ export default function Shop() {
           (
 
           <div
-            className="
-              grid
-              grid-cols-2
-              lg:grid-cols-3
-              gap-4
-              md:gap-6
-            "
-          >
+  className="
+    grid
+    grid-cols-2
+    md:grid-cols-3
+    gap-4
+    md:gap-6
+  "
+>
 
 
             {
