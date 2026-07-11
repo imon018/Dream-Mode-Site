5df7aad801b2970e67c855f1cfd2696a297c9e35
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-12 04:20:59 +0600

    Refactor SearchBar component layout and styles
    Refactor SearchBar component to improve structure and styling.

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,37 +1,162 @@
-import { useState } from "react";
+import {
+  useState,
+} from "react";
+
+import {
+  FiSearch,
+  FiX,
+} from "react-icons/fi";
+
 
 export default function SearchBar({
   onSearch,
 }) {
-  const [text, setText] =
+
+
+  const [text,setText] =
     useState("");
 
+
+
   const handleSearch = (e) => {
+
     const value =
       e.target.value;
 
     setText(value);
 
     onSearch(value);
+
   };
 
+
+
+  const clearSearch = () => {
+
+    setText("");
+
+    onSearch("");
+
+  };
+
+
+
   return (
-    <input
-      type="text"
-      value={text}
-      onChange={handleSearch}
-      placeholder="Search products..."
+
+    <div
       className="
+        relative
         w-full
-        bg-transparent
-        px-5
-        py-4
-        rounded-2xl
-        border
-        border-gray-200
-        focus:border-black
-        transition
       "
-    />
+    >
+
+
+      {/* Search Icon */}
+
+      <FiSearch
+        className="
+          absolute
+          left-5
+          top-1/2
+          -translate-y-1/2
+          text-amber-500
+          text-xl
+          md:text-2xl
+        "
+      />
+
+
+
+      <input
+
+        type="text"
+
+        value={text}
+
+        onChange={
+          handleSearch
+        }
+
+        placeholder="
+          Search your dream dress...
+        "
+
+        className="
+          w-full
+
+          h-14
+          md:h-16
+
+          pl-14
+          pr-12
+
+          rounded-3xl
+
+          bg-white
+
+          border
+          border-amber-500/20
+
+          text-sm
+          md:text-base
+
+          text-slate-800
+
+          placeholder:text-gray-400
+
+          outline-none
+
+          transition-all
+          duration-300
+
+          focus:border-amber-500
+
+          focus:ring-4
+          focus:ring-amber-500/20
+
+          shadow-sm
+
+        "
+
+      />
+
+
+
+      {
+        text && (
+
+          <button
+            onClick={
+              clearSearch
+            }
+
+            className="
+              absolute
+              right-5
+              top-1/2
+              -translate-y-1/2
+
+              text-gray-400
+
+              hover:text-amber-500
+
+              transition
+            "
+          >
+
+            <FiX
+              size={20}
+            />
+
+          </button>
+
+        )
+      }
+
+
+
+    </div>
+
   );
+
 }
