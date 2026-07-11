a97b8181b16418a31869ea1afa149cb60e4edf8c
Imon Ahmed <imon5439@gmail.com>
2026-07-11 21:34:06 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import {
-  Link,
+Link
 } from "react-router-dom";
 
 
 import {
-  FiPhone,
-  FiMail,
-  FiMapPin,
+FiPhone,
+FiMail,
+FiMapPin
 } from "react-icons/fi";
 
 
@@ -17,7 +17,6 @@ export default function Footer(){
 return (
 
 <footer
-
 className="
 bg-[#061538]
 text-white
@@ -25,168 +24,102 @@ pt-12
 pb-24
 px-6
 "
-
-
 >
 
 
-<div
-
-className="
-max-w-7xl
-mx-auto
-"
-
-
->
-
-
-
-{/* MAIN SECTION */}
+<div className="max-w-7xl mx-auto">
 
 
 
 <div
-
 className="
 grid
 grid-cols-2
 md:grid-cols-3
-gap-8
+gap-10
 "
-
-
 >
-
-
 
 
 {/* BRAND */}
 
-
-
 <div
-
 className="
 col-span-2
 md:col-span-1
 "
-
-
 >
 
 
-
 <div
-
 className="
 flex
 items-center
 gap-3
 mb-5
 "
-
-
 >
 
 
 <img
-
 src="/logo.png"
-
 className="
 w-14
 h-14
 object-contain
 "
-
 />
-
 
 
 <div>
 
 
 <h2
-
 className="
 text-2xl
 font-bold
-tracking-wide
+tracking-widest
 "
-
-
 >
 
-
-DREAM
-
-<span
-
-className="
-text-amber-500
-"
-
->
-
- MODE
-
-</span>
-
+DREAM MODE
 
 </h2>
 
 
-
 <p
-
 className="
-text-xs
 text-amber-400
+text-xs
+mt-1
 "
-
-
 >
-
 Dress Your Dream, Live Your Style
-
 </p>
 
 
 </div>
 
 
-
 </div>
 
 
 
-
-
 <p
-
 className="
 text-gray-300
-text-sm
 leading-7
+text-sm
 "
-
-
 >
 
-Dream Mode is your trusted destination for premium dress.
-We ensure quality, style and customer satisfaction.
+Dream Mode is your trusted destination for premium dress. We ensure quality, style and customer satisfaction.
 
 </p>
 
 
 
-
 </div>
-
-
-
-
 
 
 
@@ -195,21 +128,16 @@ We ensure quality, style and customer satisfaction.
 {/* QUICK LINKS */}
 
 
-
 <div>
 
 
-
 <h3
-
 className="
 text-amber-500
 font-bold
 text-lg
 mb-5
 "
-
-
 >
 
 Quick Links
@@ -217,68 +145,37 @@ Quick Links
 </h3>
 
 
-
 <ul
-
 className="
 space-y-3
 text-gray-300
 text-sm
 "
-
-
 >
 
 
 <li>
-
-<Link
-to="/"
-className="hover:text-amber-400"
->
-
+<Link to="/">
 Home
-
 </Link>
-
 </li>
 
 
-
-
 <li>
-
-<Link
-to="/shop"
-className="hover:text-amber-400"
->
-
+<Link to="/shop">
 Shop
-
 </Link>
-
 </li>
-
-
 
 
 <li>
-
-<Link
-to="/profile"
-className="hover:text-amber-400"
->
-
+<Link to="/profile">
 My Account
-
 </Link>
-
 </li>
-
 
 
 </ul>
-
 
 
 </div>
@@ -288,27 +185,19 @@ My Account
 
 
 
-
-
-
-{/* CUSTOMER SERVICE */}
-
+{/* CUSTOMER */}
 
 
 <div>
 
 
-
 <h3
-
 className="
 text-amber-500
 font-bold
 text-lg
 mb-5
 "
-
-
 >
 
 Customer Service
@@ -317,216 +206,121 @@ Customer Service
 
 
 
-
 <ul
-
 className="
 space-y-3
 text-gray-300
 text-sm
 "
-
-
 >
-
 
 
 <li>
-
-<Link
-to="/return-policy"
-className="hover:text-amber-400"
->
-
+<Link to="/page/return-policy">
 Return Policy
-
 </Link>
-
 </li>
 
 
-
-
 <li>
-
-<Link
-to="/refund-policy"
-className="hover:text-amber-400"
->
-
+<Link to="/page/refund-policy">
 Refund Policy
-
 </Link>
-
 </li>
 
 
-
-
 <li>
-
-<Link
-to="/shipping-policy"
-className="hover:text-amber-400"
->
-
+<Link to="/page/shipping-policy">
 Shipping Policy
-
 </Link>
-
 </li>
 
 
-
-
 <li>
-
-<Link
-to="/privacy-policy"
-className="hover:text-amber-400"
->
-
+<Link to="/page/privacy-policy">
 Privacy Policy
-
 </Link>
-
 </li>
-
-
 
 
 <li>
-
-<Link
-to="/terms"
-className="hover:text-amber-400"
->
-
+<Link to="/page/terms">
 Terms & Conditions
-
 </Link>
-
 </li>
-
 
 
 
 </ul>
 
 
+</div>
+
+
 
 </div>
 
 
 
-
-
-</div>
-
-
-
-
-
-
-
-
-
-{/* CONTACT + PAYMENT */}
 
 
 
 
 <div
-
 className="
 mt-12
 border-t
 border-white/10
 pt-8
 grid
-grid-cols-1
 md:grid-cols-2
 gap-8
 "
-
-
 >
-
-
 
 
 
 
 {/* CONTACT */}
 
-
-
 <div>
 
 
-
 <h3
-
 className="
 text-amber-500
 font-bold
 mb-5
 "
-
-
 >
-
 Contact Us
-
 </h3>
 
 
-
 <div
-
 className="
 space-y-4
 text-gray-300
 text-sm
 "
-
-
 >
 
 
-<p className="flex items-center gap-3">
-
-<FiPhone
-className="text-amber-500"
-/>
-
+<p className="flex gap-3">
+<FiPhone className="text-amber-500"/>
 +8801406978619
-
 </p>
 
 
-
-<p className="flex items-center gap-3">
-
-<FiMail
-className="text-amber-500"
-/>
-
+<p className="flex gap-3">
+<FiMail className="text-amber-500"/>
 dreammode27@gmail.com
-
 </p>
 
 
-
-
-<p className="flex items-center gap-3">
-
-<FiMapPin
-className="text-amber-500"
-/>
-
+<p className="flex gap-3">
+<FiMapPin className="text-amber-500"/>
 Dhaka, Bangladesh
-
 </p>
 
 
@@ -534,12 +328,7 @@ Dhaka, Bangladesh
 </div>
 
 
-
 </div>
-
-
-
-
 
 
 
@@ -547,21 +336,15 @@ Dhaka, Bangladesh
 
 {/* PAYMENT */}
 
-
-
 <div>
 
 
-
 <h3
-
 className="
 text-amber-500
 font-bold
 mb-5
 "
-
-
 >
 
 We Accept
@@ -570,79 +353,42 @@ We Accept
 
 
 
-
 <div
-
 className="
 flex
 gap-4
-flex-wrap
 items-center
+flex-wrap
 "
-
-
 >
 
 
-
 <img
-
 src="/payments/visa-logo.png"
-
-className="
-h-10
-object-contain
-"
-
+className="h-8 w-auto"
 />
 
 
-
-
-
 <img
-
 src="/payments/mastercard-logo.png"
-
-className="
-h-10
-object-contain
-"
-
+className="h-8 w-auto"
 />
 
 
-
-
-
 <img
-
 src="/payments/bkash-logo.png"
-
-className="
-h-10
-object-contain
-"
-
+className="h-8 w-auto"
 />
-
-
-
 
 
 <img
-
 src="/payments/nagad-logo.png"
-
-className="
-h-10
-object-contain
-"
-
+className="h-8 w-auto"
 />
 
 
 
+</div>
 
 
 </div>
@@ -653,25 +399,11 @@ object-contain
 
 
 
-
-</div>
-
-
-
-
-
-
-
-
-
-
-{/* COPYRIGHT */}
 
 
 
 
 <div
-
 className="
 border-t
 border-white/10
@@ -681,23 +413,16 @@ text-center
 text-gray-400
 text-sm
 "
-
-
 >
 
-
-© 2026 All Rights Reserved by DREAM MODE.
-
+© 2026 Dream Mode. All Rights Reserved by Dream Mode.
 
 </div>
 
 
 
 
-
-
 </div>
-
 
 
 </footer>
