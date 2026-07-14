c82080636e6de0462ff73e59e7dbd07b294f3b30
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-14 15:07:45 +0600

    Remove unused constants from constants.js
    Removed APP_NAME, SUPPORT_EMAIL, and SUPPORT_PHONE constants.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,3 @@
-export const APP_NAME = "Dream Mode";
-
 export const CURRENCY = "৳";
 
-export const SUPPORT_EMAIL = "dreammode27@gmail.com";
-
-export const SUPPORT_PHONE = "+8801628464209";
+export const MAX_UPLOAD_SIZE = 5;
