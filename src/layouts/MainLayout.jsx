bb3bf1113f7e1dcba9ae2479d41f5c6cd3ac302a
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-16 18:33:52 +0600

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,8 @@
-import { Outlet } from "react-router-dom";
+import {
+  Outlet,
+  useLocation,
+} from "react-router-dom";
+
 
 import AnnouncementBar from "../components/AnnouncementBar";
 import Header from "../components/Header";
@@ -6,22 +10,82 @@ import Footer from "../components/Footer";
 import MobileBottomNav from "../components/MobileBottomNav";
 import WhatsAppButton from "../components/WhatsAppButton";
 
+
+
 export default function MainLayout() {
+
+
+  const location = useLocation();
+
+
+
+  const hideBottomArea =
+    location.pathname.startsWith(
+      "/profile/orders/"
+    );
+
+
+
   return (
+
     <>
+
+
       <AnnouncementBar />
+
 
       <Header />
 
-      <main className="min-h-screen pb-20 md:pb-0">
+
+
+      <main
+        className={
+          hideBottomArea
+
+          ?
+
+          "min-h-screen"
+
+          :
+
+          "min-h-screen pb-20 md:pb-0"
+        }
+      >
+
         <Outlet />
+
       </main>
+
+
+
+
 
       <WhatsAppButton />
 
-      <MobileBottomNav />
 
-      <Footer />
+
+
+
+      {
+        !hideBottomArea && (
+
+          <>
+
+            <MobileBottomNav />
+
+            <Footer />
+
+          </>
+
+        )
+      }
+
+
+
+
+
     </>
+
   );
+
 }
