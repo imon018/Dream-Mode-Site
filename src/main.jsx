d7b756bc9c2ab31606e60cd79543ab0ccf561613
imon018 <iamon0180@gmail.com>
2026-07-26 23:40:05 +0600

    Setup live webview loading from dream-mode.shop

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { SettingsProvider } from "./context/SettingsContext";
-import { CapacitorUpdater } from "@capgo/capacitor-updater";
 
 import App from "./App";
 import "./index.css";
 import "swiper/css";
 import "swiper/css/pagination";
 
-
-CapacitorUpdater.notifyAppReady()
-  .then(() => {
-    console.log("Capgo App Ready");
-  })
-  .catch((err) => {
-    console.log("Capgo Error:", err);
-  });
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
