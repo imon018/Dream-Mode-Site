dab1ab4e05ff892b12a3fd7316951ae4b626ab1c
Imon Ahmed <imon5439@gmail.com>
2026-07-28 18:14:32 +0600

    Update updateChecker.js

--- src/utils/updateChecker.js
+++ src/utils/updateChecker.js
@@ -1,30 +1,32 @@
-import { APP_VERSION } from "./appVersion";
+import { getAppliedVersion } from "./appliedVersion";
 
 export async function checkForUpdate() {
   try {
+    const currentVersion = await getAppliedVersion();
+
     const res = await fetch(
       "https://www.dream-mode.shop/updates/version.json?t=" + Date.now()
     );
 
     const remote = await res.json();
 
-    if (remote.version !== APP_VERSION) {
+    if (remote.version !== currentVersion) {
       console.log(
         "New update available:",
         remote.version
       );
 
-      return true;
+      return { hasUpdate: true, version: remote.version };
     }
 
     console.log("App is latest version");
-    return false;
+    return { hasUpdate: false, version: remote.version };
 
   } catch (error) {
     console.log(
       "Update check failed, using local app"
     );
 
-    return false;
+    return { hasUpdate: false, version: null };
   }
 }
