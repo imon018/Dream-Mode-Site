29e06de6045002270b9beab65b41bb0489167329
Imon Ahmed <imon5439@gmail.com>
2026-08-01 22:52:14 +0600

    Enhance update checker with debug logging
    Added debug logging to track the update check process.

--- src/utils/updateChecker.js
+++ src/utils/updateChecker.js
@@ -1,14 +1,21 @@
 import { getAppliedVersion } from "./appliedVersion";
+import { pushDebugLog } from "./debugOverlay";
 
 export async function checkForUpdate() {
   try {
     const currentVersion = await getAppliedVersion();
 
+    pushDebugLog("checkForUpdate: currentVersion=" + currentVersion);
+
     const res = await fetch(
       "https://www.dream-mode.shop/updates/version.json?t=" + Date.now()
     );
 
+    pushDebugLog("checkForUpdate: fetch status=" + res.status);
+
     const remote = await res.json();
+
+    pushDebugLog("checkForUpdate: remoteVersion=" + remote.version);
 
     if (remote.version !== currentVersion) {
       console.log(
@@ -16,16 +23,21 @@ export async function checkForUpdate() {
         remote.version
       );
 
+      pushDebugLog("checkForUpdate: hasUpdate=true");
+
       return { hasUpdate: true, version: remote.version };
     }
 
     console.log("App is latest version");
+    pushDebugLog("checkForUpdate: hasUpdate=false (versions match)");
     return { hasUpdate: false, version: remote.version };
 
   } catch (error) {
     console.log(
       "Update check failed, using local app"
     );
+
+    pushDebugLog("checkForUpdate: ERROR " + (error && error.message ? error.message : String(error)));
 
     return { hasUpdate: false, version: null };
   }
