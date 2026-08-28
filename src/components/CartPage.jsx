242cd6786ba0c6ed4415e5efa50cc7b924afb7bf
Imon Ahmed <imon5439@gmail.com>
2026-08-29 01:26:27 +0600

    Add loading and decoding attributes to image

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -342,7 +342,7 @@ export default function CartPage() {
                           object-cover
                         "
 
-                      />
+                       loading="lazy" decoding="async"/>
 
 
 
